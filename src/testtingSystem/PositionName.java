c4f8e3875fa10101b95d8f04a73b7bf7f4c46267
Hoi Nguyen <hoinguyen.developer@gmail.com>
2025-06-27 01:48:34 +0700

    first

---
+++ src/testtingSystem/PositionName.java
@@ -0,0 +1,21 @@
+package testtingSystem;
+
+public enum PositionName {
+	DEV("dev"), TEST("Test"), SCRUM_MASTER("Scrum master"), PM("PM");
+	private String value;
+	PositionName(String value){
+		this.value = value;
+	}
+	public String getValue() {
+		return value;
+	}
+	// lay enum theo value
+	public static PositionName fromValue(String value) {
+		for(PositionName positionName: PositionName.values()) {
+			if(positionName.getValue().equals(value)) {
+				return positionName;
+			}
+		}
+		return null;
+	}
+}
